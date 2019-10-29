72313b68103f45649ea5842f4231cdfd0742089c
ITStan <s.stanislavskaya@gmail.com>
2019-10-29 23:28:53 +0200

    Updates

---
+++ precache-manifest.81948ec47a8aa2de4abc1b384b5cef34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a38d8e3490b606fe3d2c1e6d573b887",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "f8b2bad2561f26ac0a17",
+    "url": "/react_static-list-of-posts/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_static-list-of-posts/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "f8b2bad2561f26ac0a17",
+    "url": "/react_static-list-of-posts/static/js/main.b1d21453.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
